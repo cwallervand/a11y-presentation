4e6d9494c68d7bda53a816807d72dc1c3569b3bd
Christian Wallervand <cwallervand@gmail.com>
2020-12-16 15:43:35 +0100

    Started on Slide 1

---
+++ a11y-presentation/src/Slide1.jsx
@@ -0,0 +1,16 @@
+
+const Slide1 = () => (
+  <div>
+    <h1>Accessability in practice</h1>
+    <p>
+      Welcome to this introduction to accessability in practice for developers.
+      This session will cover some basics and some more advanced techniques for those fancy user experiences.
+      At the end of this session you will hopefully have learned something new (or just refreshed your memory)
+    </p>
+    <p>
+      Lets get into it!
+    </p>
+  </div>
+);
+
+export default Slide1;
